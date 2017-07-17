14c1afeb9748e65313549c23fbc4beca91a27be8
seunzone <darealseun@gmail.com>
2017-07-18 00:13:06 +0100

    Interface Design

---
+++ views/results.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- CSS FILES -->
+<link rel="stylesheet" href="../css/main.css"/>
+
+</head>
+
+
+<body>
+	<div class="myDiv" align="center">
+		<h1>Your Score is</h1>
+		<h2><span name = "score">3 </span> out of <span name = "total"> 5 </span> </h2>
+			
+	</div>
+</body>
+
+</html> 

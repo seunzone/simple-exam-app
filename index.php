ec231da575b1e60df92dd07b608b4818cebc0075
seunzone <darealseun@gmail.com>
2017-07-17 12:40:28 +0100

    Interface Design

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- CSS FILES -->
+<link rel="stylesheet" href="css/main.css"/>
+
+</head>
+
+
+<body>
+	<div class="myDiv" align="center">
+		<h1>Click Start Once You're Ready to Start Your Exams</h1>
+			<form action="views/exam.php" method="POST">
+				
+				<br><button class="myButton">Start</button>
+			</form>
+	</div>
+</body>
+
+</html> 
